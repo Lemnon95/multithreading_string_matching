30019faa468f6867dea91ae9d578505ce0fcac32
battisti98 <battisti.1848458@studenti.uniroma1.it>
2020-12-05 16:10:34 +0100

    tcp procedure added in serial programm

--- serial.c
+++ serial.c
@@ -17,6 +17,62 @@ struct UDP_hdr {
 	u_short	uh_sum;		/* datagram checksum */
 };
 
+/* TCP structs */
+
+/* Ethernet header */
+struct sniff_ethernet {
+	u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+	u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+	u_short ether_type; /* IP? ARP? RARP? etc */
+};
+
+/* IP header */
+struct sniff_ip {
+	u_char ip_vhl;		/* version << 4 | header length >> 2 */
+	u_char ip_tos;		/* type of service */
+	u_short ip_len;		/* total length */
+	u_short ip_id;		/* identification */
+	u_short ip_off;		/* fragment offset field */
+	#define IP_RF 0x8000		/* reserved fragment flag */
+	#define IP_DF 0x4000		/* don't fragment flag */
+	#define IP_MF 0x2000		/* more fragments flag */
+	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+	u_char ip_ttl;		/* time to live */
+	u_char ip_p;		/* protocol */
+	u_short ip_sum;		/* checksum */
+	struct in_addr ip_src,ip_dst; /* source and dest address */
+};
+#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+	u_short th_sport;	/* source port */
+	u_short th_dport;	/* destination port */
+	tcp_seq th_seq;		/* sequence number */
+	tcp_seq th_ack;		/* acknowledgement number */
+	u_char th_offx2;	/* data offset, rsvd */
+	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+	u_char th_flags;
+	#define TH_FIN 0x01
+	#define TH_SYN 0x02
+	#define TH_RST 0x04
+	#define TH_PUSH 0x08
+	#define TH_ACK 0x10
+	#define TH_URG 0x20
+	#define TH_ECE 0x40
+	#define TH_CWR 0x80
+	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+	u_short th_win;		/* window */
+	u_short th_sum;		/* checksum */
+	u_short th_urp;		/* urgent pointer */
+};
+
+#define UDP 0
+#define	TCP 1
+
 /* Reports a problem with dumping the packet with the given timestamp. */
 void problem_pkt(struct timeval ts, const char *reason);
 
@@ -25,6 +81,9 @@ void too_short(struct timeval ts, const char *truncated_hdr);
 
 /* Procedure that reads a UDP packet and prints its payload content */
 const unsigned char* dump_UDP_packet(const unsigned char *packet, struct timeval ts, unsigned int capture_len);
+
+/* Procedure that reads a TCP packet and prints its payload content */
+const unsigned char* dump_TCP_packet(const unsigned char *packet);
 
 /*Knuth-Morris-Pratt String Matching Algorithm's functions.*/
 int kmp_matcher (char text[], char pattern[]);
@@ -38,8 +97,22 @@ int main(int argc, char *argv[]) {
 	char errbuf[PCAP_ERRBUF_SIZE];
 	struct pcap_pkthdr header;
 	char *filepath;
-	if (argc == 2) {
+
+	int packet_type = UDP; //default udp
+	
+	if (argc==2 || argc ==3) { 
 		filepath = argv[1]; //get filename from command-line
+		
+		if(argc == 3) { //get packet type from command-line
+			if(strcmp(argv[2], "udp") == 0)
+				packet_type=UDP;
+			else if (strcmp(argv[2], "tcp") == 0)
+				packet_type=TCP;
+			else {
+				printf("USAGE ./serial <file.pcap> [tcp/udp]\n");
+				exit(1);
+			}
+		}
 	}
 	else {
 		printf("USAGE: ./serial <file.pcap>\n");
@@ -59,8 +132,12 @@ int main(int argc, char *argv[]) {
 	
 	/* Loop extracting packets as long as we have something to read */
 	while ((packet = pcap_next(pcap, &header)) != NULL) {
-		
-		const unsigned char* payload = dump_UDP_packet(packet, header.ts, header.caplen); //getting the payload
+		const unsigned char* payload;
+		if(packet_type == UDP) //udp
+			payload = dump_UDP_packet(packet, header.ts, header.caplen); //getting the payload
+		else //tcp
+			payload = dump_TCP_packet(packet); //getting the payload
+			
 		if(payload != NULL) {
 			for (int i = 0; i < size_S; i++) 
 				string_count[i] += kmp_matcher((char *)payload,S[i]);
@@ -134,20 +211,6 @@ const unsigned char* dump_UDP_packet(const unsigned char *packet, struct timeval
 		return NULL;
 	}
 
-	
-	/* Now the packet points to the upd_header, that contains:
-	*  source port, destination port, packet lenght and checksum. */
-	//udp_header = (struct UDP_hdr*) packet;
-
-	//Erase comment to print the packet info
-	/*
-	printf("UDP src_port=%d dst_port=%d length=%d checksum=%d\n",
-		ntohs(udp_header->uh_sport),
-		ntohs(udp_header->uh_dport),
-		ntohs(udp_header->uh_ulen),
-		ntohs(udp_header->uh_sum));
-	*/
-
 	/*	According to the UDP standard the header is 32 bit lenght
 	*	Adding 32 bit to the packet we have a pointer to the payload */
 	const unsigned char* payload = packet + 32;
@@ -155,6 +218,44 @@ const unsigned char* dump_UDP_packet(const unsigned char *packet, struct timeval
 	//printf("payload= \n%s\n", payload);
 	
 	return payload;
+}
+
+const unsigned char* dump_TCP_packet(const unsigned char *packet) {
+	/* ethernet headers are always exactly 14 bytes */
+	#define SIZE_ETHERNET 14
+
+	const struct sniff_ethernet *ethernet; // The ethernet header 
+	const struct sniff_ip *ip; // The IP header 
+	const struct sniff_tcp *tcp; // The TCP header 
+	const unsigned char* payload; // Packet payload 
+
+	u_int size_ip;
+	u_int size_tcp;
+
+
+	ethernet = (struct sniff_ethernet*)(packet);
+	packet += SIZE_ETHERNET; //move packet pointer adding the ethernet size to get the ip pointer
+	
+	ip = (struct sniff_ip*)(packet); 
+	size_ip = IP_HL(ip)*4;
+	if (size_ip < 20) {
+		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+		return NULL;
+	}
+	
+	packet += size_ip; //move packet pointer adding the ethernet size to get the tcp pointer
+	tcp = (struct sniff_tcp*)(packet);
+	size_tcp = TH_OFF(tcp)*4;
+	if (size_tcp < 20) {
+		printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+		return NULL;
+	}
+	
+	packet += size_tcp; //move packet pointer adding the tcp size to get the payload pointer
+	payload = (u_char *)(packet);
+	
+	return payload;
+
 }
 
 
